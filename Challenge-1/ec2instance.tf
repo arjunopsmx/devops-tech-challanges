6a2790310efaedd8b0ff7293d7ecd62c4d4b50c3
Arjun P <74755521+arjunopsmx@users.noreply.github.com>
2023-01-18 12:04:54 +0530

    changed instance resource name

--- Challenge-1/ec2instance.tf
+++ Challenge-1/ec2instance.tf
@@ -1,5 +1,5 @@
 # Creating EC2 instance one in Public Subnet
-resource "aws_instance" "demoinstance" {
+resource "aws_instance" "mywebappinstance1" {
   ami                         = "ami-087c17d1fe0178315"
   instance_type               = "t2.micro"
   count                       = 1
@@ -15,7 +15,7 @@ resource "aws_instance" "demoinstance" {
 }
 
 # Creating EC2 instance two in Public Subnet
-resource "aws_instance" "demoinstance1" {
+resource "aws_instance" "mywebappinstance2" {
   ami                         = "ami-087c17d1fe0178315"
   instance_type               = "t2.micro"
   count                       = 1
